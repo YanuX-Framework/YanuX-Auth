00e80fa25ee64027da202a86b4e32884c80a0363
petersaints <petersaints@gmail.com>
2018-04-26 17:46:50 +0100

    Bug fixes

--- utils/httpbearerstrategy.js
+++ utils/httpbearerstrategy.js
@@ -4,11 +4,11 @@ var HttpBearerStrategy = require('passport-http-bearer').Strategy
 var AccessToken = require('../models/accesstoken');
 
 module.exports = new HttpBearerStrategy((accessToken, callback) => {
-    AccessToken.findOne({ value: accessToken }).populate('user').exec()
+    AccessToken.findOne({ token: accessToken }).populate('user').exec()
         .then(token => {
             if (token && token.user) {
                 // TODO: Implement Scopes!
-                callback(null, user, { scope: '*' });
+                callback(null, token.user, { scope: '*' });
             } else {
                 return callback(null, false);
             }
