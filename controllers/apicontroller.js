2268f78fac3e7f88398d4a04cd153e9bd09c8177
petersaints <petersaints@gmail.com>
2018-08-07 20:37:28 +0100

    - Webpack and Mongoose updated - Minor changes to the OAuth2 verification API (There are still things that I want to change about, such as returning JSON even when I get a 401 ERRO, but for now it will do!)

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -3,17 +3,20 @@
 const jwt = require('jsonwebtoken');
 
 exports.verifyOAuth2 = function (req, res, next) {
-    jwt.sign({
-        user: req.user
-    }, req.app.get('config').keys.private_key,
+    const response = {
+        user: {
+            email: req.user.email
+        }
+    };
+    jwt.sign(response, req.app.get('config').keys.private_key,
         { algorithm: 'RS256' },
         function (err, token) {
             if (err) {
                 next(err)
             } else {
                 res.json({
-                    user: req.user,
-                    token: token
+                    response: response,
+                    jwt: token
                 });
             }
         });
