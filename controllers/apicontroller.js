33d1454c6f8f56c0a3bd11bc332469b9b4227020
petersaints <petersaints@gmail.com>
2018-08-09 00:10:44 +0100

    - HttpBearerStrategy now provides more that is accessible from route handlers/controllers.

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -3,11 +3,7 @@
 const jwt = require('jsonwebtoken');
 
 exports.verifyOAuth2 = function (req, res, next) {
-    const response = {
-        user: {
-            email: req.user.email
-        }
-    };
+    const response = req.authInfo;
     jwt.sign(response, req.app.get('config').keys.private_key,
         { algorithm: 'RS256' },
         function (err, token) {
--- utils/httpbearerstrategy.js
+++ utils/httpbearerstrategy.js
@@ -4,11 +4,26 @@ var HttpBearerStrategy = require('passport-http-bearer').Strategy
 var AccessToken = require('../models/accesstoken');
 
 module.exports = new HttpBearerStrategy((accessToken, callback) => {
-    AccessToken.findOne({ token: AccessToken.hashToken(accessToken) }).populate('user').exec()
+    AccessToken.findOne({
+        token: AccessToken.hashToken(accessToken),
+        expirationDate: { $gt: new Date() }
+    }).populate('user')
+        .populate('client')
+        .exec()
         .then(token => {
-            if (token && token.user) {
-                // TODO: Implement Scopes!
-                callback(null, token.user, { scope: '*' });
+            if (token && token.user && token.client) {
+                // TODO: Properly Implement Scopes and other "advanced" details!
+                callback(null, token.user, {
+                    user: {
+                        email: token.user.email
+                    }, client: {
+                        name: token.client.name,
+                        id: token.client.id
+                    }, access_token: {
+                        expiration_date: token.expirationDate,
+                        scope: token.scope
+                    }
+                });
             } else {
                 return callback(null, false);
             }
