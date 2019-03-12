4bf8d8abf61ed87713530c9bcb4ab7ecea163de7
petersaints <petersaints@gmail.com>
2019-03-12 20:06:11 +0000

    - Added Zeroconf Network Advertisement

---
+++ zeroconf.js
@@ -0,0 +1,6 @@
+const dnssd = require('dnssd');
+
+module.exports = function (app) {
+    const ad = new dnssd.Advertisement(dnssd.tcp('http'), app.get('port'), { name: app.get('config').name, txt: { protocol: 'http' }});
+    ad.start();
+}
