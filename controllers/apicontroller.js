92288a7a7a54ca844b73e2cc0a6b4a3617a2686a
petersaints <petersaints@gmail.com>
2019-09-26 18:30:39 +0100

    - Nothing worth mentioning

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -4,16 +4,9 @@ const jwt = require('jsonwebtoken');
 
 exports.verifyOAuth2 = function (req, res, next) {
     const response = req.authInfo;
-    jwt.sign(response, req.app.get('config').keys.private_key,
-        { algorithm: 'RS256' },
-        function (err, token) {
-            if (err) {
-                next(err)
-            } else {
-                res.json({
-                    response: response,
-                    jwt: token
-                });
-            }
-        });
+    jwt.sign(response, req.app.get('config').keys.private_key, { algorithm: 'RS256' }, (err, token) => {
+        if (err) { next(err) } else {
+            res.json({ response: response, jwt: token });
+        }
+    });
 }
